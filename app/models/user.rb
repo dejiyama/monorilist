68dd47a5695c5270e75ee4a7d77a5aa50e145924
jiro.kirameki <deziyama0404@gmail.com>
2017-11-15 15:54:48 +0000

    have

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,26 @@ class User < ApplicationRecord
   
   has_many :wants
   has_many :want_items, through: :wants, class_name: 'Item', source: :item
+  has_many :haves, class_name: 'Have'
+  has_many :have_items, through: :haves, class_name: 'Item', source: :item
   has_secure_password
   
+  def have(item)
+    self.haves.find_or_create_by(item_id: item.id)
+  end
+  
+  def unhave(item)
+    have = self.haves.find_by(item_id: item.id)
+    have.destroy if have
+  end
+  
+  def have?(item)
+    self.have_items.include?(item)
+  end
+  
+  
   def want(item)
-    self.wants.find_or_created_by(item_id: item.id)
+    self.wants.find_or_create_by(item_id: item.id)
   end
   
   def unwant(item)
