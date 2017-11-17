898df9c99dc600788b1c27f892218b0ca1e8361d
jiro.kirameki <deziyama0404@gmail.com>
2017-11-17 00:29:53 +0000

    monolist

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -6,6 +6,6 @@ class RankingsController < ApplicationController
   
   def have
     @ranking_counts = Have.ranking
-    @items = Item.find(@ranking_counts.key)
+    @items = Item.find(@ranking_counts.keys)
   end
 end
